ec8a9428c9cfcda1497642159ade08b38db2223f
James Paik <jpaik456@gmail.com>
2019-01-30 16:39:09 -0500

    Fix to reply handler

--- routes/sms.js
+++ routes/sms.js
@@ -15,29 +15,31 @@ router.post('/', (req, res) => {
   console.dir(reply);
 
   let message = '';
-  if(reply.indexOf('NAME') > -1) { //Most likely setting their name
+  if (reply.indexOf('NAME') > -1) { //Most likely setting their name
     let name = req.app.reminders.setName(number, reply);
     message = "Nice to meet you, " + name + "! From now on, I'll remind you to eat :)";
     reply = name;
   } else { //Replying to script
-    if(reply.length > 2) {
+    if (reply.length > 2) {
       reply = reply.split(' ');
       if (reply.indexOf('NO') > -1) {
         reply = 'NO';
-      } else if(reply.indexOf('ATE') > -1) {
+      } else if (reply.indexOf('ATE') > -1) {
         reply = 'ATE';
       } else {
         reply = 'invalid';
       }
-      message = req.app.reminders.handleReply(reply, number);
     }
+    message = req.app.reminders.handleReply(reply, number);
   }
 
   let number = req.body.From;
   console.dir(number + ': ' + reply);
   const twiml = new MessagingResponse();
   twiml.message(message);
-  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.writeHead(200, {
+    'Content-Type': 'text/xml'
+  });
   res.end(twiml.toString());
 });
 
@@ -49,8 +51,8 @@ router.post('/send', (req, res) => {
   let pw = req.body.password;
   let number = req.body.number;
 
-  if(pw && number) {
-    if(pw === process.env.PASSWORD) {
+  if (pw && number) {
+    if (pw === process.env.PASSWORD) {
       req.app.reminders.sendToNumber(number);
       req.app.reminders.setReminder("NO", number);
       res.status(200).send();
